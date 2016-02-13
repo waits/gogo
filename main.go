5274d29ffbb5bd9f300e31d91d803d1108caac72
Dylan Waits <dylan@waits.io>
2016-02-13 15:15:19 -0800

    Enable TLS when --cert argument is supplied

--- main.go
+++ main.go
@@ -10,9 +10,11 @@ import (
 )
 
 var (
-	httpAddr     = flag.String("http", "localhost:8080", "HTTP listen address")
+	httpAddr     = flag.String("http", "127.0.0.1:8080", "HTTP listen address")
 	templatePath = flag.String("template", "template/", "path to template files")
 	staticPath   = flag.String("static", "static/", "path to static files")
+	cert         = flag.String("cert", "", "path to TLS certificate")
+	key          = flag.String("key", "", "path to private key")
 	reload       = flag.Bool("reload", false, "reload templates for every request")
 )
 
@@ -35,5 +37,13 @@ func main() {
 	http.Handle("/watch/", reqHandler{c, watchHandler})
 	http.Handle("/static/", http.FileServer(http.Dir("./")))
 	http.Handle("/live/game/", websocket.Handler(liveHandler))
-	http.ListenAndServe(*httpAddr, nil)
+	var err error
+	if *cert != "" {
+		err = http.ListenAndServeTLS(*httpAddr, *cert, *key, nil)
+	} else {
+		err = http.ListenAndServe(*httpAddr, nil)
+	}
+	if err != nil {
+		log.Panic(err)
+	}
 }
