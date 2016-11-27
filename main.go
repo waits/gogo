49b592adaf502044f2916ffd3ca59f9a37de2d0b
Dylan Waits <dylan@waits.io>
2016-11-26 18:00:20 -0800

    Handle TLS directly in app instead of offloading

--- main.go
+++ main.go
@@ -13,8 +13,10 @@ import (
 
 var (
 	db       = flag.Int("db", 0, "Redis database number")
-	httpAddr = flag.String("http", "localhost:8080", "HTTP listen address")
+	addr     = flag.String("addr", "localhost:8080", "Address to listen on")
 	reload   = flag.Bool("reload", false, "reload templates for every request")
+	certFile = flag.String("cert", "", "path to certificate chain")
+	keyFile  = flag.String("key", "", "path to private key")
 )
 
 func main() {
@@ -26,10 +28,14 @@ func main() {
 	env := &handler.Env{Templates: t, TemplatePath: "template/"}
 	model.InitPool(*db)
 
-	log.Printf("Starting server at http://%s\n", *httpAddr)
+	log.Printf("Starting server at http://%s\n", *addr)
 	http.Handle("/", handler.Handler{Env: env, Fn: handler.StaticHandler})
 	http.Handle("/game/", handler.Handler{Env: env, Fn: handler.GameHandler})
 	http.Handle("/static/", http.FileServer(http.Dir("./")))
 	http.Handle("/live/game/", websocket.Handler(handler.LiveHandler))
-	http.ListenAndServe(*httpAddr, nil)
+
+	if *certFile != "" && *keyFile != "" {
+		log.Fatal(http.ListenAndServeTLS(*addr, *certFile, *keyFile, nil))
+	}
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
